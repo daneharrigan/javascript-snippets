27c2bedbf3087a2960b6ea98156e9d26f87d35eb
Dane Harrigan <dane.harrigan@gmail.com>
2010-07-18 19:53:18 -0400

    typo: too many each loops

--- jquery/add_active_class_to_link_of_current_page.js
+++ jquery/add_active_class_to_link_of_current_page.js
@@ -1,12 +1,10 @@
 $('a[href]').each(function(){
   var url_length = 0
-  $('a[href]').each(function(){
-    if(window.location.toString().match(this.href) && this.href.length>url_length)
-    {
-      $('a.active').removeClass('active')
-      $(this).addClass('active')
-      ul_length = this.href.length
-    }
-  })
+  if(window.location.toString().match(this.href) && this.href.length>url_length)
+  {
+    $('a.active').removeClass('active')
+    $(this).addClass('active')
+    ul_length = this.href.length
+  }
 });
 
